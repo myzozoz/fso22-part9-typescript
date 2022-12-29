2c16be782c0284dc807e9e425bfb4e38deb5a2df
Markus Holopainen <markusp_holopainen@yahoo.com>
2022-12-29 10:54:39 +0200

    9.2

---
+++ 1-7/exerciseCalculator.ts
@@ -0,0 +1,51 @@
+interface Exercise {
+  totalDays: number;
+  trainingDays: number;
+  target: number;
+  avg: number;
+  targetReached: boolean;
+  rating: number;
+  description: string;
+}
+
+const calculateExercises = (input: Array<number>, target: number): Exercise => {
+  const totalHours = input.reduce(
+    (prev: number, curr: number) => prev + curr,
+    0
+  );
+  console.log(`Total: ${totalHours}`);
+
+  const trainingDays = input.reduce(
+    (prev: number, curr: number) => (curr > 0 ? prev + 1 : prev),
+    0
+  );
+  console.log(`Total: ${trainingDays}`);
+
+  const avg = totalHours / input.length;
+
+  // Rating is 1 if target is reached, 2 if the average is still over two thirds of the target and 3 otherwise
+  let rating = 0;
+  let description = "";
+  if (avg > target) {
+    rating = 1;
+    description = "Very well done, target reached!";
+  } else if (avg / target > 0.66) {
+    rating = 2;
+    description = "Not bad, but room for improvement.";
+  } else {
+    rating = 3;
+    description = "You'll have to work harder to reach your goals!.";
+  }
+
+  return {
+    totalDays: input.length,
+    trainingDays,
+    target,
+    avg,
+    targetReached: avg >= target,
+    rating,
+    description,
+  };
+};
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
